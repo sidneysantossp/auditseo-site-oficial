27920266f05ba3a72599d71c575d8e4ce05c510a
sidneysantossp <73052036+sidneysantossp@users.noreply.github.com>
2026-08-04 07:09:51 -0300

    Sync update for src/routes/solucoes/recuperacao-organica.tsx from Lovable

---
+++ src/routes/solucoes/recuperacao-organica.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/solucoes/recuperacao-organica')({
+  component: () => <div className="p-20 text-white">Página em reestruturação individual: Recuperação orgânica</div>
+})
