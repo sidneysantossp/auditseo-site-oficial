6ccb1e3a9c0c0fe968d694fcf80d487ba5dcb76c
sidneysantossp <73052036+sidneysantossp@users.noreply.github.com>
2026-08-04 07:09:52 -0300

    Sync update for src/routes/solucoes/site-sem-tracao.tsx from Lovable

---
+++ src/routes/solucoes/site-sem-tracao.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/solucoes/site-sem-tracao')({
+  component: () => <div className="p-20 text-white">Página em reestruturação individual: Sites no ar sem tração</div>
+})
