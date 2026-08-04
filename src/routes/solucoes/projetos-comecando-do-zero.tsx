3b629e586508c1a10e1c9d1ccaedb8634ec91a5f
sidneysantossp <73052036+sidneysantossp@users.noreply.github.com>
2026-08-04 07:09:49 -0300

    Sync update for src/routes/solucoes/projetos-comecando-do-zero.tsx from Lovable

---
+++ src/routes/solucoes/projetos-comecando-do-zero.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/solucoes/projetos-comecando-do-zero')({
+  component: () => <div className="p-20 text-white">Página em reestruturação individual: Projetos começando do zero</div>
+})
