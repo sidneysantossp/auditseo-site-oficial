e7616f13942459c641fe0b3008c0119f2be21980
sidneysantossp <73052036+sidneysantossp@users.noreply.github.com>
2026-08-04 07:09:41 -0300

    Sync update for src/routes/solucoes/autoridade-de-entidade.tsx from Lovable

---
+++ src/routes/solucoes/autoridade-de-entidade.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/solucoes/autoridade-de-entidade')({
+  component: () => <div className="p-20 text-white">Página em reestruturação individual: Autoridade de entidade</div>
+})
