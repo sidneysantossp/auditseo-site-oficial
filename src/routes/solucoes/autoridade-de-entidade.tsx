b919589de9a719fc2666a706b17c7b28724fab02
sidneysantossp <73052036+sidneysantossp@users.noreply.github.com>
2026-09-07 16:58:08 -0300

    feat(content): connect Entity Authority to authority evidence

--- src/routes/solucoes/autoridade-de-entidade.tsx
+++ src/routes/solucoes/autoridade-de-entidade.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import ServiceLandingPage from "@/components/ServiceLandingPage";
+import ServiceLandingPageV2 from "@/components/ServiceLandingPageV2";
 import { servicePages } from "@/content/services";
 import { createServiceHead } from "@/lib/serviceSeo";
 
@@ -7,5 +7,5 @@ const data = servicePages.authority;
 
 export const Route = createFileRoute("/solucoes/autoridade-de-entidade")({
   head: () => createServiceHead(data),
-  component: () => <ServiceLandingPage data={data} />,
+  component: () => <ServiceLandingPageV2 data={data} />,
 });
